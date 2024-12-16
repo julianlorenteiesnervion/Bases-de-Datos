4333447ebebd07cfc936f077c9cd9459bfd31878
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2024-12-16 10:19:47 +0100

    Create Ejercicios 2 al 18.sql

---
+++ Tema-5/Ejercicios 2 al 18.sql
@@ -0,0 +1,65 @@
+/* BASE DE DATOS EJERCICIO 2 */
+CREATE DATABASE Ej2
+GO
+USE Ej2
+GO
+
+CREATE TABLE Empleados (
+	DNI tinyInt,
+	Constraint PK_DNI Primary Key (DNI)
+)
+
+CREATE TABLE Departamentos (
+	NUMDPTO int,
+	Constraint PK_NUMDPTO Primary Key (NUMDPTO)
+)
+
+CREATE TABLE Deptos_Empleados (
+	DNI_EMPLEADO tinyInt,
+	NUM_DPTO int,
+	Constraint FK_DNI_EMPLEADO FOREIGN KEY (DNI_EMPLEADO) REFERENCES Empleados (DNI),
+	Constraint FK_NUM_DPTO FOREIGN KEY (NUM_DPTO) REFERENCES Departamentos (NUMDPTO)
+)
+
+/* BASE DE DATOS EJERCICIO 3 */
+CREATE DATABASE Ej3
+GO
+USE Ej3
+GO
+
+CREATE TABLE FUTBOLISTAS (
+	dni tinyInt,
+	Constraint PK_FUTBOLISTAS Primary Key (dni)
+)
+
+CREATE TABLE PARTIDOS (
+	id int,
+	fecha date,
+	Constraint PK_PARTIDOS Primary Key (id)
+)
+
+CREATE TABLE PARTIDOS_JUGADORES (
+	id int,
+	dni tinyInt,
+	CONSTRAINT FK_ID_PARTIDOS_JUGADORES FOREIGN KEY (id) REFERENCES PARTIDOS (id),
+	CONSTRAINT FK_DNI_PARTIDOS_JUGADORES FOREIGN KEY (dni) REFERENCES FUTBOLISTAS (dni)
+)
+
+/* BASE DE DATOS EJERCICIO 4 */
+CREATE DATABASE Ej4
+GO
+USE Ej4
+GO
+
+CREATE TABLE CLIENTES (
+	dni tinyInt,
+	Constraint PK_CLIENTES Primary Key (dni)
+)
+
+CREATE TABLE COMPRAS (
+	dni tinyInt,
+	modelo int,
+	fecha date,
+	unidades tinyInt,
+
+)
