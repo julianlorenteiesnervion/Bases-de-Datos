04a638098601ae8f5d76702cb502ccbada035ce2
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-02-06 10:20:55 +0100

    Create Bichos.sql

---
+++ Tema-7/Bichos.sql
@@ -0,0 +1,11 @@
+USE Bichos
+GO
+
+-- Introduce dos nuevos clientes. Asígnales los códigos que te parezcan adecuados
+INSERT INTO BI_Clientes VALUES (107, 612345987, 'Calle Corta, 12', NULL, 'Pablo Iglesias')
+INSERT INTO BI_Clientes VALUES (108, 678987987, 'Calle Utrera, 11', NULL, 'José Manuel')
+
+-- Introduce una mascota para cada uno de ellos. Asígnales los códigos que te parezcan adecuados
+INSERT INTO BI_Mascotas VALUES ('PP123', 'Ibérico', 'Cerdo', '2020-11-22', NULL, 'Pata Negra', 107)
+INSERT INTO BI_Mascotas VALUES ('PP321', 'Alemán', 'Gato', '2021-05-10', NULL, 'Oreo', 108)
+
