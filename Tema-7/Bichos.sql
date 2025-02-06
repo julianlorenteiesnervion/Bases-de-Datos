23cfa5c1bc1e2c2665565c1bcacb52088285271a
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-02-06 11:44:57 +0100

    Update Bichos.sql

--- Tema-7/Bichos.sql
+++ Tema-7/Bichos.sql
@@ -9,3 +9,5 @@ INSERT INTO BI_Clientes VALUES (108, 678987987, 'Calle Utrera, 11', NULL, 'José 
 INSERT INTO BI_Mascotas VALUES ('PP123', 'Ibérico', 'Cerdo', '2020-11-22', NULL, 'Pata Negra', 107)
 INSERT INTO BI_Mascotas VALUES ('PP321', 'Alemán', 'Gato', '2021-05-10', NULL, 'Oreo', 108)
 
+-- Escribe un SELECT para obtener los IDs de las enfermedades que ha sufrido alguna mascota. Los IDs no deben repetirse
+SELECT IDEnfermedad FROM BI_Mascotas_Enfermedades
