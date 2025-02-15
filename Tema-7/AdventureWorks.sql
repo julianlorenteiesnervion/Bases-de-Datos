dd0f729934e058d5735a59e069bfbafb548c838e
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-02-15 12:07:26 +0100

    Update AdventureWorks.sql

--- Tema-7/AdventureWorks.sql
+++ Tema-7/AdventureWorks.sql
@@ -17,4 +17,20 @@ FROM SalesLT.Product
 WHERE SellStartDate <= '20020901' AND (SellEndDate IS NULL OR SellEndDate >= '20020930') AND [Weight] < 2000
 
 -- Margen de beneficio de cada producto (Precio de venta menos el coste), y porcentaje que supone respecto del precio de venta
-select * from
+SELECT ProductID, (ListPrice - StandardCost) AS 'Beneficio', ((ListPrice - StandardCost) * 100) / StandardCost AS 'Porcentaje'
+FROM SalesLT.Product
+
+-- Número de productos de cada categoría
+SELECT ProductCategoryID AS 'Categoría', COUNT(ProductID) AS 'Nº Productos'
+FROM SalesLT.Product
+GROUP BY ProductCategoryID
+
+-- Cantidad de subcategorías que hay dentro de cada categoría padre
+SELECT ParentProductCategoryID AS 'C. Padre', COUNT(ProductCategoryID) AS 'Subcategoría'
+FROM SalesLT.ProductCategory
+GROUP BY ParentProductCategoryID
+
+-- Número de unidades vendidas de cada producto cada año
+SELECT ProductID AS 'Producto', YEAR(ModifiedDate) AS 'Año', SUM(OrderQty) AS 'Unds. Vendidas'
+FROM SalesLT.SalesOrderDetail
+GROUP BY ProductID, YEAR(ModifiedDate)
