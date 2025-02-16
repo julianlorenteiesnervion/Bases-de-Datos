f08fa15c1eee3b6b58a303c49a7085072ca1a47c
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-02-16 17:18:38 +0100

    Update AdventureWorks.sql

--- Tema-7/AdventureWorks.sql
+++ Tema-7/AdventureWorks.sql
@@ -34,3 +34,43 @@ GROUP BY ParentProductCategoryID
 SELECT ProductID AS 'Producto', YEAR(ModifiedDate) AS 'Año', SUM(OrderQty) AS 'Unds. Vendidas'
 FROM SalesLT.SalesOrderDetail
 GROUP BY ProductID, YEAR(ModifiedDate)
+
+-- 1. Nombre completo (Title, nombre, inicial del segundo nombre, apellidos) de todos los clientes que en los nombres de sus compañías aparezcan las palabras "cycle” o "bike”
+SELECT Title, FirstName, MiddleName, LastName
+FROM SalesLT.Customer
+WHERE CompanyName LIKE ('%cycle%') OR CompanyName LIKE ('%bike%')
+
+-- 2. Repite la consulta anterior sin incluir la inicial del nombre. ¿Obtienes la misma información? ¿A qué es debido?
+-- Respuesta: Sí que obtengo la misma información, ya que no tenemos ninguna condición en la consulta que dependa de la inicial del nombre
+SELECT Title, FirstName, LastName
+FROM SalesLT.Customer
+WHERE CompanyName LIKE ('%cycle%') OR CompanyName LIKE ('%bike%')
+
+-- 4. Número de productos de cada color
+SELECT Color, COUNT(ProductID) AS 'Nº Productos'
+FROM SalesLT.Product
+GROUP BY Color
+
+-- 5. El margen de un producto es la diferencia entre su precio de venta (ListPrice) y su precio de coste (StandardPrice). Crea una consulta que obtenga nombre y número del producto, margen y categoría
+SELECT Name, ProductID, ListPrice - StandardCost AS 'Margin', ProductCategoryID
+FROM SalesLT.Product
+
+-- 6. ID de categoría y margen medio de los productos de esa categoría. Ten en cuenta que el margen medio es la media de los márgenes
+SELECT ProductCategoryID, SUM(ListPrice - StandardCost) / COUNT(ListPrice) AS 'Margen medio'
+FROM SalesLT.Product
+GROUP BY ProductCategoryID
+
+-- 7. Consulta cuantas direcciones diferentes tenemos de cada país
+SELECT CountryRegion, COUNT(AddressID) AS 'Direcciones'
+FROM SalesLT.Address
+GROUP BY CountryRegion
+
+-- 8. Consulta cuantas direcciones diferentes tenemos de cada país y estado
+SELECT CountryRegion, StateProvince, COUNT(AddressID) AS 'Direcciones'
+FROM SalesLT.Address
+GROUP BY CountryRegion, StateProvince
+
+-- 9. Consulta cuantas direcciones diferentes tenemos de cada país, estado y ciudad
+SELECT CountryRegion, StateProvince, City, COUNT(AddressID) AS 'Direcciones'
+FROM SalesLT.Address
+GROUP BY CountryRegion, StateProvince, City
